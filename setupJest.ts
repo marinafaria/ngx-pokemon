644fbac543ca91788b84feb9d9de149b0a0933e1
Marina Faria <marina.faria@localiza.com>
2021-02-09 16:12:39 -0300

    Pokemon card test

--- setupJest.ts
+++ setupJest.ts
@@ -1,2 +1,3 @@
 import 'jest-preset-angular';
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
--- src/app/modules/home/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/modules/home/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,19 +1,21 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { of } from "rxjs";
 import { PokedexService } from "src/app/core/services/pokedex.service"
 import { PokemonCardComponent } from "./pokemon-card.component"
+import { render, RenderResult } from '@testing-library/angular'
 
 describe('PokemonCardComponent', () => {
   let component: PokemonCardComponent;
   let fixture: ComponentFixture<PokemonCardComponent>;
-  beforeEach( async ( () => { 
-    TestBed.configureTestingModule({
+  let renderResult: RenderResult<PokemonCardComponent>;
+  beforeEach( async () => { 
+    renderResult = await render(PokemonCardComponent, {
       providers: [
         {
           provide: PokedexService,
           useValue: {
             getPokemonByName: () => of ({
-              name: 'localiza',
+              name: 'pikachu',
               abilities: [],
               game_indices: [],
               sprite: 'img.jpeg',
@@ -24,24 +26,50 @@ describe('PokemonCardComponent', () => {
       ],
       declarations: [
         PokemonCardComponent
-      ]
-  }).compileComponents();
-    fixture = TestBed.createComponent(PokemonCardComponent);
+      ],
+      componentProperties: {
+        pokedex: { 
+          name: 'pikachu',
+          url: 'google.com'
+        }
+      }
+  })
+    fixture = renderResult.fixture;
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }))
+  })
 
   it('should create', () => {
-    component.pokedex = { 
-      name: 'calculadora',
-      url: 'google.com'
-    }
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
+    const { container } = renderResult;
+    expect(container).toBeTruthy();
   })
   
   it('should render pokemon name', () => {
-    const text = fixture.nativeElement.querySelector('.pokemon-name');
-    expect(text.innerHTML).toContain('localiza');
+    const { getByText } = renderResult;
+    expect(getByText('pikachu')).toBeInTheDocument();
   })
+
+  it('should load img', () => {
+    const { getByAltText } = renderResult;
+    expect(getByAltText('pikachu')).toHaveAttribute('src', 'img.jpeg');
+  }) 
+
+  it('should load placeholder if there isnt a img', () => {
+    const { getByAltText } = renderResult;
+    component.pokemon = {
+      ...component.pokemon, 
+      sprite: null
+    }
+    fixture.detectChanges();
+    expect(getByAltText('pikachu')).toHaveAttribute('src', '/assets/pokeball.jpg');
+  })
+
+  it('should emit when a pokemon is clicked', () => {
+    component.onPokemonClick.emit = jest.fn();
+    const { container } = renderResult;
+    const card = container.querySelector('.card-container') as HTMLElement;
+    card.click();
+    expect(component.onPokemonClick.emit).toBeCalledWith(component.pokedex);
+  }) 
 })
+
