3339f713cb5e2e04322dbdd8861179fbcee48e01
Marina <maarina.faria@gmail.com>
2023-08-28 17:54:46 -0300

    refactor: search bar using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {        
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem'
+      }
+    },
   },
   plugins: [],
 }
